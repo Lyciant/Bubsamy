8a35a40ba78e245230830a0a717de5595913d2fe
Lyciant <lyciant.em@gmail.com>
2022-05-05 11:26:57 +0200

    Uploading all project files
    Its a brand new 3rd person template project file.

---
+++ Source/Bubsamy/Bubsamy.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Bubsamy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Bubsamy, "Bubsamy" );
+ 
